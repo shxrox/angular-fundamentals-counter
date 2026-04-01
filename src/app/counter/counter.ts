f1533cd80ea352ca83d523355dab78c386729458
shxrox <sharondeva8@gmail.com>
2026-04-01 17:50:07 +0530

    angular-fundamentals-counter

---
+++ src/app/counter/counter.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.html',
+  styleUrl: './counter.css',
+})
+export class Counter {
+
+  count=0;
+
+  increment(){
+    this.count++;
+  }
+
+  decrement(){
+    this.count--;
+  }
+
+  reset(){
+    this.count=0;
+  }
+
+
+}
